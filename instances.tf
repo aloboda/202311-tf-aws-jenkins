5d446c84f63e62887a1a13fe7be486d47d456809
Andrii Loboda <andrii.loboda@gmail.com>
2023-11-21 23:00:29 -0500

    Add provisioner to install packages no one need.

--- instances.tf
+++ instances.tf
@@ -32,6 +32,13 @@ resource "aws_instance" "jenkins-master" {
   depends_on = [
     aws_main_route_table_association.set-master-default-rt-assoc
   ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+EOF
+  }
 }
 
 
@@ -55,4 +62,12 @@ resource "aws_instance" "jenkins-worker-oregon" {
     Name = join("_", ["jenkings-worker-oregon-tf", count.index + 1])
   }
   depends_on = [aws_main_route_table_association.set-worker-rt-association, aws_instance.jenkins-master]
+
+
+  provisioner "local-exec" {
+    command = <<EOF
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
+EOF
+  }
 }
